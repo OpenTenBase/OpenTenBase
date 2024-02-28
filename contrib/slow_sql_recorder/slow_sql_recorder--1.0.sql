4f5e1e010932ef0ff520e0382458b42ed8fbb05a
Kaijie Gu <2459548460@qq.com>
2024-02-28 11:23:47 +0800

    2.28

--- contrib/slow_sql_recorder/slow_sql_recorder--1.0.sql
+++ contrib/slow_sql_recorder/slow_sql_recorder--1.0.sql
@@ -1,12 +1,12 @@
 \echo Use "CREATE EXTENSION pg_testgen" to load this file. \quit
 
-CREATE FUNCTION record()
+CREATE FUNCTION record(integer)
     RETURNS integer
     AS 'MODULE_PATHNAME', 'record'
     LANGUAGE C STRICT;
 
 CREATE FUNCTION report()
-    RETURNS integer
+    RETURNS SETOF varchar 
     AS 'MODULE_PATHNAME', 'report'
     LANGUAGE C STRICT;
     
--- contrib/slow_sql_recorder/slow_sql_recorder.h
+++ contrib/slow_sql_recorder/slow_sql_recorder.h
@@ -1,2 +1,16 @@
+#ifndef SLOW_SQL_RECORDER_H
+#define SLOW_SQL_RECORDER_H
+
 #include "postgres.h"
 #include "fmgr.h"
+#include <limits.h>
+
+#include "libpq/auth.h"
+#include "port.h"
+#include "utils/guc.h"
+#include "utils/timestamp.h"
+#include "commands/explain.h"
+#include "executor/instrument.h"
+#include "utils/guc.h"
+
+#endif
