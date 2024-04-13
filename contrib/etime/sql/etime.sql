332b4ddc710b5b807942a064487ca6166211c4df
QlQl <2458371920@qq.com>
2024-04-13 21:56:54 +0800

    change unit test

--- contrib/etime/sql/etime.sql
+++ contrib/etime/sql/etime.sql
@@ -1,11 +1,16 @@
+drop table if exists etime cascade;
+create table etime (sql text, time bigint);
+
+-- create extension
+drop extension if exists etime;
 create extension etime;
 load '$libdir/etime';
-
-create table etime (sql text, time bigint);
 set etime.tablename = "etime";
 set etime.min_value = 1000000;
 set etime.max_sql_size = 128;
 
+
+drop table if exists foo cascade;
 create table foo(id bigint, str text) distribute by shard(id);
 insert into foo values(1, 'tencent'), (2, 'shenzhen');
 
@@ -15,10 +20,20 @@ insert into foo values(1, 'tencent'), (2, 'shenzhen');
 insert into foo select 1, 'asdasd';
 
 -- It cannot be recorded due to fast execution.
-select * from foo;
+select count(*) from foo;
 
 -- It can be recorded for its execution time more than 1s you set above.
 select pg_sleep(1);
 
--- output record
-select * from etime;
+-- output record count is 1
+select count(*) from etime;
+
+-- change threshold
+set etime.min_value = 2000000;
+
+-- output record count is still 1
+select pg_sleep(1);
+select count(*) from etime;
+
+drop table if exists etime cascade;
+drop table if exists foo cascade;
